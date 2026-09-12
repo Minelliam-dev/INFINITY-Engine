a126edd82b1e746287014ccd442d01d470581882
Minelliam-dev <Minelliam536@gmail.com>
2026-09-12 12:33:53 +0200

    Added more scene functionality

--- obj/Debug/net10.0/INFINITY_Engine_3D.AssemblyInfo.cs
+++ obj/Debug/net10.0/INFINITY_Engine_3D.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INFINITY_Engine_3D")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10d9d3f5fa7a4ab8a1d81cc990d593ad9c19ef63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8917a37aab517f5790e63787a73d7320382cf6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("INFINITY_Engine_3D")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INFINITY_Engine_3D")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
